2de633bbf4a551f1befbcfbba04c24e6ef929967
zhao-jiunhao <62795877+zhao-jiunhao@users.noreply.github.com>
2021-01-28 17:19:59 +0800

    Add files via upload

--- computer/src/mathsComputer/sjhs.java
+++ computer/src/mathsComputer/sjhs.java
@@ -1,7 +1,13 @@
 package mathsComputer;
 
 public class sjhs extends ddxs implements kys {
-
+	public static final int sin=1;
+	public static final int cos=2;
+	public static final int tan=3;
+	private int ang;
+	public sjhs(int c,int a) {
+		
+	}
 	@Override
 	public void hj() {
 		// TODO Auto-generated method stub
